f74a75d254ae77746205b67cfbf251d68dde2fcb
RolandoReis <rolando.reis@estudante.ifms.edu.br>
2025-04-24 14:10:50 -0400

    Update exercicio_03.c

--- exercicio_03.c
+++ exercicio_03.c
@@ -1,4 +1,7 @@
-
+// int main(): Função principal do programa, onde a execução começa.
+// int a, b, c;: Declara três variáveis inteiras chamadas a, b e c.
+// printf("Insira um valor para a, b e c:\n");: Mostra uma mensagem pedindo ao usuário para inserir três números.
+// scanf("%d %d %d", &a, &b, &c);: Lê três valores inteiros do teclado e os armazena nas variáveis a, b e c.
 
 
 
