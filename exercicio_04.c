4a9cc7253c5e34648273153150e2af73cca1c6b8
RolandoReis <rolando.reis@estudante.ifms.edu.br>
2025-04-24 15:08:36 -0400

    Update exercicio_04.c

--- exercicio_04.c
+++ exercicio_04.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int isBissexto(int ano) {
+    return (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
+}
+
+int main() {
+    int ano1, ano2;
+
+    printf("Digite o primeiro ano: ");
+    scanf("%d", &ano1);
+    printf("Digite o segundo ano: ");
+    scanf("%d", &ano2);
+
+    if (ano1 >= ano2) {
+        printf("O primeiro ano deve ser menor que o segundo.\n");
+        return 1;
+    }
+    printf("Anos bissextos entre %d e %d:\n", ano1, ano2);
+
+    for (int ano = ano1 + 1; ano < ano2; ano++) {
+        if (isBissexto(ano)) {
+            printf("%d\n", ano);
+      }
+    }
+
+    return 0;
+}
