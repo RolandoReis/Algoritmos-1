a87a86bfbbb5b7cf4684b38e12c475cbf62d9766
RolandoReis <rolando.reis@estudante.ifms.edu.br>
2025-04-24 11:19:28 -0400

    Add files via upload

---
+++ 01.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main (){
+    float a, b, c;
+
+    printf("digite os três lados do triâmgulo:\n");
+
+    scanf("%f %f %f", &a, &b, &c);
+
+    if(a + b > c && a + c > b && b + c > a) {
+
+        printf("É um triângulo.\n");
+
+        if (a == b && b == c) {
+            printf("Tipo: Equilátero\n");
+
+        } else if (a == b || a == c || b == c) {
+
+            printf("Tipo: Isóseles\n");
+        } else {
+
+            printf("Tipo: Escaleno\n");
+        }
+    } else { 
+        printf("Os valores fornecidos não formam um triângulo.\n");
+    }
+
+    return 0;
+}
