ec1ee1065fab77c9d44ea7f3784dd2c4c116df65
RolandoReis <rolando.reis@estudante.ifms.edu.br>
2026-08-27 09:04:31 -0400

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+    int a, b;
+    printf("Digite dois números diferentes: \n");
+    scanf("%d %d", &a,&b);
+
+
+    if (a > b){
+        printf("%d É MAIOR!\n", a);
+
+    } else {
+        printf("%d É MAIOR!\n", b);
+
+
+    }
+
+
+
+return 0;
+
+
+}
