5b1202766c540db2ce3ec7ac3448d9aab0597470
RolandoReis <rolando.reis@estudante.ifms.edu.br>
2026-09-16 21:36:43 -0400

    Update LIsta_III_01.c

--- LIsta_III_01.c
+++ LIsta_III_01.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main() {
+
+    int numero;
+
+    printf("Digite um numero: ");
+    scanf("%d", &numero);
+
+    while (numero != 0) {
+
+        printf("Quadrado: %d\n", numero * numero);
+
+        printf("Digite outro numero: ");
+        scanf("%d", &numero);
+    }
+
+    return 0;
+}
